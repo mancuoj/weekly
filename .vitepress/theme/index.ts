ffd81899fb9758b7b00be5994811cdaca51f2ad5
mancuoj <mancuoj@gmail.com>
2024-09-23 21:56:10 +0800

    feat: update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,13 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import CustomLayout from './layouts/CustomLayout.vue'
+import Figure from './components/Figure.vue'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
   Layout: CustomLayout,
-  enhanceApp({ app, router, siteData }) {},
+  enhanceApp({ app, router, siteData }) {
+    app.component('Figure', Figure)
+  },
 } satisfies Theme
